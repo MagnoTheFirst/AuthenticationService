15ab78506ca00ea6a3711947ad8187c7e4c78600
alejandro.laneri@students.ffhs.ch <alejandro.laneri@students.ffhs.ch>
2021-10-02 14:02:16 +0200

    First Lady

---
+++ src/main/java/com/example/authenticationservicerolethedice/AuthenticationServiceRoleTHeDiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticationservicerolethedice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceRoleTHeDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServiceRoleTHeDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/authenticationservicerolethedice/AuthenticationServiceRoleTHeDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authenticationservicerolethedice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceRoleTHeDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
